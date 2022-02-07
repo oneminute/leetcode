016ec4a5047aa94c81e939f0cbb5c7018f037fe8
OneMinute <oneminute3b@gmail.com>
2022-02-08 00:40:22 +0800

    Improve codes.

--- problems/longest-palindromic-substring/sol01.cpp
+++ problems/longest-palindromic-substring/sol01.cpp
@@ -23,41 +23,31 @@ using namespace std;
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        if (n < 2) 
-        {
+        int strLength = s.size();
+        if (strLength < 2) 
             return s;
-        }
 
-        int maxLen = 1; // 最长回文子串的长度
+        int maxLength = 1; // 最长回文子串的长度
         int begin = 0;  // 最长回文子串的位置
 
         // dp[i][j] 表示对于输入的s来说，从索引i到索引j位置的子串是否是回文串
-        vector<vector<int>> dp(n, vector<int>(n));
+        vector<vector<int>> dp(strLength, vector<int>(strLength, 0));
 
         // 初始化：所有长度为 1 的子串都是回文串
-        for (int i = 0; i < n; i++) 
-        {
-            dp[i][i] = true;
-        }
+        for (int i = 0; i < strLength; i++) 
+            dp[i][i] = 1;
 
         // 递推开始。从左向右，先判定第一个2字符的子串是否是回文子串。依次增加要判定的回文字串的长度，直到原文字串长度为止
-        for (int L = 2; L <= n; L++) {
-            // 枚举左边界，左边界的上限设置可以宽松一些
-            for (int i = 0; i < n; i++) {
-                // 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
-                int j = L + i - 1;
-                // 如果右边界越界，就可以退出当前循环
-                if (j >= n) 
-                {
-                    break;
-                }
+        for (int subLength = 2; subLength <= strLength; subLength++) {
+            for (int i = 0; i < strLength - subLength; i++) {
+                // 计算子串的右边界
+                int j = i + subLength - 1;
 
                 if (s[i] == s[j])   // 如果首尾字符相等
                 {
-                    if (j - i < 3)  // 如果子串长度小于3 
+                    if (subLength < 3)  // 如果子串长度小于3 
                     {
-                        dp[i][j] = true;
+                        dp[i][j] = 1;
                     }
                     else    // 如果子串长度大于3，当前子串的回文状态取决于掐头去尾的子串的回文状态
                     {
@@ -66,27 +56,27 @@ public:
                 }
                 else    // 首尾字符不相等 
                 {
-                    dp[i][j] = false;
+                    dp[i][j] = 0;
                 }
 
                 // 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
-                if (dp[i][j] && j - i + 1 > maxLen) 
+                if (dp[i][j] && subLength > maxLength) 
                 {
-                    maxLen = j - i + 1;
+                    maxLength = subLength;
                     begin = i;
                 }
             }
         }
-        return s.substr(begin, maxLen);
+        return s.substr(begin, maxLength);
     }
 };
 
 int main()
 {
     st::LeetCodeTestRunner<string> runner;
-    st::LeetCodeTestCase<string> test01("test01", "bab", [=]() {
+    st::LeetCodeTestCase<string> test01("test01", "a", [=]() {
         Solution sol;
-        return sol.longestPalindrome("babad");
+        return sol.longestPalindrome("a");
     });
     runner.addTask(&test01);
     runner.start();
