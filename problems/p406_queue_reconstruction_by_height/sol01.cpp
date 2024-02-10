745857c9affc6d5ddb3b90f7f6cfd65f37cc7d4d
oneminute3b <oneminute3b@gmail.com>
2024-02-10 09:34:49 -0600

    add p406

--- problems/p406_queue_reconstruction_by_height/sol01.cpp
+++ problems/p406_queue_reconstruction_by_height/sol01.cpp
@@ -11,29 +11,52 @@ using namespace std;
 
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [=] (vector<int>& i1, vector<int>& i2) {
-            return i1[1] < i2[1];
-        });
-        int count = 0;
-        int prev = intervals[0][1];
-        for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0] < prev) {
-                count++;
+    vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
+        sort(people.begin(), people.end(), [=] (vector<int>& i1, vector<int>& i2) {
+            if (i1[1] < i2[1]) {
+                return true;
+            } else if (i1[1] > i2[1]) {
+                return false;
             } else {
-                prev = intervals[i][1];
+                return (i1[0] > i2[0]);
             }
+        });
+
+        list<vector<int>> queue;
+        queue.push_back(people[0]);
+        for (int i = 1; i < people.size(); i++) {
+            int height = people[i][0];
+            int count = people[i][1];
+            list<vector<int>>::iterator it = queue.begin();
+            for (; it != queue.end(); it++) {
+                if (height <= it->at(0)) {
+                    count--;
+                }
+                if (count < 0)
+                    break;
+            }
+            queue.insert(it, people[i]);
         }
-        return count;
+
+        int i = 0;
+        for (list<vector<int>>::iterator it = queue.begin(); it != queue.end(); it++) {
+            people[i][0] = it->at(0);
+            people[i][1] = it->at(1);
+            i++;
+        }
+
+        return people;
     }
 };
 
 int main()
 {
     Solution s;
-    // vector<vector<int>> intervals = {{1,2}, {2,3}, {3,4}, {1,3}};
-    vector<vector<int>> intervals = {{1,2}, {1,2}, {1,2}};
-    int total = s.eraseOverlapIntervals(intervals);
-    printf("total: %d", total);
+    vector<vector<int>> people = {{7,0},{4,4},{7,1},{5,0},{6,1},{5,2}};
+    people = s.reconstructQueue(people);
+    for (int i = 0; i < people.size(); i++) {
+        printf("[%d, %d], ", people[i][0], people[i][1]);
+    }
+    printf("\n");
     return 0;
 }
