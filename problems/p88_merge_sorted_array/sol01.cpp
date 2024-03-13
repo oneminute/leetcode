ebdb20e1a2819ec343ac1a53881a258bd6caeb17
oneminute3b <oneminute3b@gmail.com>
2024-03-13 14:31:04 -0500

    update p88

--- problems/p88_merge_sorted_array/sol01.cpp
+++ problems/p88_merge_sorted_array/sol01.cpp
@@ -14,19 +14,13 @@ public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         int i1 = m - 1;
         int i2 = n - 1;
-        for (int i = nums1.size() - 1; i >= 0; i--) {
-            int n; 
-            if (i2 < 0)
-                break;
-            else if (i1 < 0) {
-                nums1[i] = nums2[i2--];
-            } else if (nums1[i1] >= nums2[i2]) {
-                nums1[i] = nums1[i1];
-                i1--;
-            } else if (i1 < 0 || nums1[i1] < nums2[i2]) {
-                nums1[i] = nums2[i2];
-                i2--;
-            }
+
+        int i = nums1.size() - 1;
+        while (i1 >= 0 && i2 >= 0) {
+            nums1[i--] = nums1[i1] > nums2[i2] ? nums1[i1--] : nums2[i2--];
+        }
+        while (i2 >= 0) {
+            nums1[i--] = nums2[i2--];
         }
     }
 };
