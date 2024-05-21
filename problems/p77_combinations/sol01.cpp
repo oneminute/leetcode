9be4d7863bb1706eb7e10caeb1919f1119c3cb77
oneminute3b <oneminute3b@gmail.com>
2024-05-21 14:29:14 -0500

    add p77

--- problems/p77_combinations/sol01.cpp
+++ problems/p77_combinations/sol01.cpp
@@ -17,22 +17,24 @@ int dirs[5] = {-1, 0, 1, 0, -1};
 class Solution
 {
 public:
-    void dfs(vector<int>& nums, int level, vector<vector<int>>& results) {
-        if (level >= nums.size()) {
-            results.push_back(nums);
+    void dfs(vector<int>& items, int& level, int index, int n, int k, vector<vector<int>>& results) {
+        if (level == k) {
+            results.push_back(items);
             return;
         }
 
-        for (int i = level; i < nums.size(); i++) {
-            swap(nums[i], nums[level]);
-            dfs(nums, level + 1, results);
-            swap(nums[i], nums[level]);
+        for (int i = index; i <= n; i++) {
+            items[level++] = i;
+            dfs(items, level, i + 1, n, k, results);
+            level--;
         }
     }
 
-    vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> combine(int n, int k) {
         vector<vector<int>> results;
-        dfs(nums, 0, results);
+        vector<int> items(k, 0);
+        int level = 0;
+        dfs(items, level, 1, n, k, results);
         return results;
     }
 };
@@ -41,8 +43,7 @@ int main()
 {
     Solution s;
     {
-        vector<int> nums = {1,2,3};
-        vector<vector<int>> results = s.permute(nums);
+        vector<vector<int>> results = s.combine(4, 2);
         for (int i = 0; i < results.size(); i++) {
             cout << "[";
             int j = 0;
@@ -54,21 +55,7 @@ int main()
         }
     }
     {
-        vector<int> nums = {0,1};
-        vector<vector<int>> results = s.permute(nums);
-        for (int i = 0; i < results.size(); i++) {
-            cout << "[";
-            int j = 0;
-            for (; j < results[i].size() - 1; j++) {
-                cout << results[i][j] << ", ";
-            }
-            cout << results[i][j];
-            cout << "]" << endl;
-        }
-    }
-    {
-        vector<int> nums = {1};
-        vector<vector<int>> results = s.permute(nums);
+        vector<vector<int>> results = s.combine(1, 1);
         for (int i = 0; i < results.size(); i++) {
             cout << "[";
             int j = 0;
